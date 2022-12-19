0f89b98ea3bfe519d4b18c75a4fdebef497cf628
ArtemOsipchuk <artyom.osipchuk92@gmail.com>
2022-12-19 19:39:35 +0200

    initial commit hw#26

---
+++ src/ui/TemperaturesList.js
@@ -0,0 +1,11 @@
+export class TemperaturesList {
+    //TODO similar to EmployeesList
+    constructor(idList) {
+        //TODO
+    }
+    showTemperatures(dataArray) {
+        //TODO
+        //{city, objects: [{date,hour,temperature},...]}
+    }
+}
+Footer
